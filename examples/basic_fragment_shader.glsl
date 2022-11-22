3414789df624ecdf518eb20cd74d6bc6c88127f4
Kravantokh <mamaia7777@gmail.com>
2022-11-23 00:46:55 +0200

    Added a bezier example with geometry shader.

--- examples/basic_fragment_shader.glsl
+++
@@ -1,8 +0,0 @@
-#version 410 core
-
-out vec4 FragColor;
-
-void main()
-{
-	FragColor = vec4(0.5, 0.5, 0.5, 1);
-}
--- examples/uniform_fragment_shader.glsl
+++
@@ -1,9 +0,0 @@
-#version 410 core
-
-out vec4 FragColor;
-uniform vec4 Color;
-
-void main()
-{
-	FragColor = Color;
-}
